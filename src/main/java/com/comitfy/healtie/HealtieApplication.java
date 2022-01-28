a9cae8700f87f156d5d77afa48e0658ae52123a2
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2022-01-28 14:10:04 +0300

    initial commit

---
+++ src/main/java/com/comitfy/healtie/HealtieApplication.java
@@ -0,0 +1,13 @@
+package com.comitfy.healtie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealtieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealtieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comitfy/healtie/HealtieApplicationTests.java
@@ -0,0 +1,14 @@
+package com.comitfy.healtie;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealtieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
