1773fb8799cae9f601f364810472d4044cc929a9
furkanyalcindag <furkanyalcindag@hotmail.com>
2022-02-28 08:55:47 +0300

    pageable repo

---
+++ src/main/java/com/comitfy/healtie/component/EmailServiceImpl.java
@@ -0,0 +1,12 @@
+package com.comitfy.healtie.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceImpl   {
+
+
+
+
+
+}
--- src/main/java/com/comitfy/healtie/repository/ArticleRepository.java
+++ src/main/java/com/comitfy/healtie/repository/ArticleRepository.java
@@ -1,7 +1,23 @@
 package com.comitfy.healtie.repository;
 
 import com.comitfy.healtie.entity.Article;
+import com.comitfy.healtie.entity.Doctor;
+import com.comitfy.healtie.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Set;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    List<Article> findAll(Pageable pageable);
+
+    List<Article> findAllByDoctor(Doctor doctor, Pageable pageable);
+
+    List<Article> findAllByPinUsers(Set<User> pinUsers, Pageable pageable);
+
+    Integer countAllById(Long id);
+
+
 }
