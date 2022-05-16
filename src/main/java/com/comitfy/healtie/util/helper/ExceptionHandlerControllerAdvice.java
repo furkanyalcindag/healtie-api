06e7983a2b4fa84510800e3c1c94177ce0dbe43b
furkanyalcindag <Oxit2016>
2022-05-16 14:06:39 +0300

    Article like structure

--- src/main/java/com/comitfy/healtie/util/helper/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/comitfy/healtie/util/helper/ExceptionHandlerControllerAdvice.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class ExceptionHandlerControllerAdvice {
+public class ExceptionHandlerControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
