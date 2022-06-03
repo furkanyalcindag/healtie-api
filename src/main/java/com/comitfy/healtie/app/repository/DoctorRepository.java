6dedfd42987d4cfde2201d540ffcc7fab8d99915
BuketEdir <buketcokelek@gmail.com>
2022-06-03 14:11:28 +0300

    Doctor-Article-Count

--- src/main/java/com/comitfy/healtie/app/repository/DoctorRepository.java
+++ src/main/java/com/comitfy/healtie/app/repository/DoctorRepository.java
@@ -2,10 +2,15 @@ package com.comitfy.healtie.app.repository;
 
 import com.comitfy.healtie.app.entity.Doctor;
 import com.comitfy.healtie.util.common.BaseRepository;
-import com.comitfy.healtie.util.common.BaseWithMultiLanguageRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DoctorRepository extends BaseRepository<Doctor> {
+
+/*
+    @Query("SELECT COUNT(articles) FROM Doctor doctor" +
+            " inner join doctor.articleList articles where doctor.uuid=?1")
+    long getCountOfArticles(UUID doctorUUID);
+*/
 
 }
